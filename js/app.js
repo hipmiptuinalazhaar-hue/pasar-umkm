9c1b272e185c80ba7e44d22c255080a71a48bc49
hipmiptuinalazhaar-hue <hipmiptuinalazhaar@gmail.com>
2026-08-31 22:00:37 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1980,6 +1980,12 @@ function runAction(action, element) {
         );
         break;
 
+      case 'seller-message':
+        openSellerMessage(
+       element.dataset.storeId
+        );
+        break;
+
       case 'seller-contact':
         openSellerContact(
        element.dataset.storeId
