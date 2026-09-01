8194c01066c7b401a19d0f387317e2e6fd67f503
hipmiptuinalazhaar-hue <hipmiptuinalazhaar@gmail.com>
2026-09-01 22:55:22 +0700

    feat: hubungkan API media foto profil

--- src/worker-entry.js
+++ src/worker-entry.js
@@ -1,8 +1,16 @@
 import legacyWorker from "./worker.js";
 import { handleProfileApi } from "./profile-api.js";
+import { handleProfileMediaApi } from "./profile-media-api.js";
 
 export default {
   async fetch(request, env, ctx) {
+    const profileMediaResponse =
+      await handleProfileMediaApi(request, env);
+
+    if (profileMediaResponse) {
+      return profileMediaResponse;
+    }
+
     const profileResponse =
       await handleProfileApi(request, env);
 
