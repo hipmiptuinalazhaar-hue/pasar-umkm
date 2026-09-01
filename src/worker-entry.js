eff104cd8db7f6816eb1f227645eab672a449050
hipmiptuinalazhaar-hue <hipmiptuinalazhaar@gmail.com>
2026-09-01 23:12:39 +0700

    feat: hubungkan API profil publik ke worker

--- src/worker-entry.js
+++ src/worker-entry.js
@@ -1,9 +1,17 @@
 import legacyWorker from "./worker.js";
 import { handleProfileApi } from "./profile-api.js";
 import { handleProfileMediaApi } from "./profile-media-api.js";
+import { handlePublicProfileApi } from "./public-profile-api.js";
 
 export default {
   async fetch(request, env, ctx) {
+    const publicProfileResponse =
+      await handlePublicProfileApi(request, env);
+
+    if (publicProfileResponse) {
+      return publicProfileResponse;
+    }
+
     const profileMediaResponse =
       await handleProfileMediaApi(request, env);
 
